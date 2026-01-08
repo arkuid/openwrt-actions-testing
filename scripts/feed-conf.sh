194ed5a709d59c8c0a28e61a5173c84fdc58dbeb
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-08 20:30:01 +0500

    Update feed-conf.sh

--- scripts/feed-conf.sh
+++ scripts/feed-conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "src-git awgopenwrt https://github.com/this-username-has-been-taken/amneziawg-openwrt" >> feeds.conf.default
+#echo "src-git awgopenwrt https://github.com/this-username-has-been-taken/amneziawg-openwrt" >> feeds.conf.default
 
 
-git clone https://github.com/openwrt/packages.git
-rm -r feeds/packages/lang/golang
-cp -r packages/lang/golang feeds/packages/lang
+#git clone https://github.com/openwrt/packages.git
+#rm -r feeds/packages/lang/golang
+#cp -r packages/lang/golang feeds/packages/lang
