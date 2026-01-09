14357136f422a596df1ce38d91a5976f433e824e
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-09 11:12:32 +0500

    Update commonscripts.sh

--- scripts/commonscripts.sh
+++ scripts/commonscripts.sh
@@ -3,10 +3,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
 
-#echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20=y" >> .config
-#echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=y" >> .config
-#echo "CONFIG_PACKAGE_kmod-crypto-chacha20poly1305=y" >> .config
-#echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
-#echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
-#echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
-#echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
+echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20=y" >> .config
+echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=y" >> .config
+echo "CONFIG_PACKAGE_kmod-crypto-chacha20poly1305=y" >> .config
+echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
+echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
+echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
+echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
