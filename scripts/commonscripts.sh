9709cc7aea531e9add2f857f48226f23c9593f78
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-09 18:40:10 +0500

    Update commonscripts.sh

--- scripts/commonscripts.sh
+++ scripts/commonscripts.sh
@@ -2,10 +2,11 @@
 #
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
-
-echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20=y" >> .config
-echo "CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=y" >> .config
-echo "CONFIG_PACKAGE_kmod-crypto-chacha20poly1305=y" >> .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-kpp=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-curve25519=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-chacha20poly1305=/s/=m$/=y/' .config
 echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
 echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
 echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
