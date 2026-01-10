9171cba40f9dfb73072289f2cbf47b507b802915
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-10 14:23:40 +0500

    Update commonscripts.sh

--- scripts/commonscripts.sh
+++ scripts/commonscripts.sh
@@ -2,15 +2,12 @@
 #
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
-#sed -i '/^CONFIG_PACKAGE_kmod-crypto-kpp=/s/=m$/=y/' .config
-#sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-curve25519=/s/=m$/=y/' .config
-#sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20=/s/=m$/=y/' .config
-#sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=/s/=m$/=y/' .config
-#sed -i '/^CONFIG_PACKAGE_kmod-crypto-chacha20poly1305=/s/=m$/=y/' .config
-#echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
-#echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
-#echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
-#echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
-#git clone https://github.com/openwrt/packages.git
-#rm -r feeds/packages/lang/golang
-#cp -r packages/lang/golang feeds/packages/lang
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-kpp=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-curve25519=/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-udptunnel4/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-udptunnel6/s/=m$/=y/' .config
+sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=/s/=m$/=y/' .config
+echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
+echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
+echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
+echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
