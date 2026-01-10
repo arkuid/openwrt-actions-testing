1c28157d4ab6c65af999deb9ce040572bccb9d4b
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-10 23:17:37 +0500

    Update customscript.sh

--- tplink/c20v4/v24/customscript.sh
+++ tplink/c20v4/v24/customscript.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+for p in *.patch; do patch -p1 -d feeds/packages < "$p"; done
 
-cat > tp20v4-org-16mb-usb.patch << 'EOF'
+cat > mktplinkfw2.patch << 'EOF'
 --- a/tools/firmware-utils/patches/001-16mb-mktplinkfw2.patch
 +++ b/tools/firmware-utils/patches/001-16mb-mktplinkfw2.patch
 @@ -0,0 +1,15 @@
@@ -21,14 +22,12 @@ cat > tp20v4-org-16mb-usb.patch << 'EOF'
 + 		.fw_max_len	= 0x770000,
 
 EOF
-patch -p1 < tp20v4-org-16mb-usb.patch
+patch -p1 < mktplinkfw2.patch
 
 sed -i 's/IMAGE_SIZE := 7808k/IMAGE_SIZE := 16000k/; s/TPLINK_FLASHLAYOUT := 8Mmtk/TPLINK_FLASHLAYOUT := 16Mmtk/; s/DEVICE_PACKAGES := kmod-mt76x0e/DEVICE_PACKAGES := kmod-mt76x0e kmod-usb2 kmod-usb-ohci/' target/linux/ramips/image/mt76x8.mk
 sed -i '/&ehci\|&ohci/{n;s/status = "disabled"/status = "okay"/}' target/linux/ramips/dts/mt7628an_tplink_archer-c20-v4.dts
 sed -i 's/7a0000/fa0000/; s/7c/fc/g; s/7d/fd/g' target/linux/ramips/dts/mt7628an_tplink_8m.dtsi
 
-
-for p in *.patch; do patch -p1 -d feeds/packages < "$p"; done
 
 
 
