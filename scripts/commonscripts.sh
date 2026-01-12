179b3b1a815798fd7a78b72b98acf7c146db1064
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-12 11:44:44 +0500

    Update commonscripts.sh

--- scripts/commonscripts.sh
+++ scripts/commonscripts.sh
@@ -2,14 +2,17 @@
 #
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
-sed -i '/^CONFIG_PACKAGE_kmod-crypto-kpp=/s/=m$/=y/' .config
-sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-curve25519=/s/=m$/=y/' .config
-sed -i '/^CONFIG_PACKAGE_kmod-udptunnel4/s/=m$/=y/' .config
-sed -i '/^CONFIG_PACKAGE_kmod-udptunnel6/s/=m$/=y/' .config
-sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=/s/=m$/=y/' .config
-echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
-echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
-echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
-echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-zapret2=y" >> .config
-echo "CONFIG_PACKAGE_zapret2=y" >> .config
+#sed -i '/^CONFIG_PACKAGE_kmod-crypto-kpp=/s/=m$/=y/' .config
+#sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-curve25519=/s/=m$/=y/' .config
+#sed -i '/^CONFIG_PACKAGE_kmod-udptunnel4/s/=m$/=y/' .config
+#sed -i '/^CONFIG_PACKAGE_kmod-udptunnel6/s/=m$/=y/' .config
+#sed -i '/^CONFIG_PACKAGE_kmod-crypto-lib-chacha20poly1305=/s/=m$/=y/' .config
+
+#sed -i 's/^# CONFIG_PACKAGE_libnetfilter-queue is not set$/CONFIG_PACKAGE_libnetfilter-queue=y/; s/^CONFIG_PACKAGE_libnetfilter-queue=m$/CONFIG_PACKAGE_libnetfilter-queue=y/' .config
+
+#echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> .config
+#echo "CONFIG_PACKAGE_amneziawg-go=n" >> .config
+#echo "CONFIG_PACKAGE_amneziawg-tools=y" >> .config
+#echo "CONFIG_PACKAGE_luci-proto-amneziawg=y" >> .config
+#echo "CONFIG_PACKAGE_luci-app-zapret2=y" >> .config
+#echo "CONFIG_PACKAGE_zapret2=y" >> .config
