51fe91d65185facf64a09de111e04fd1f36be37a
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-12 13:50:21 +0500

    Update feed-conf.sh

--- scripts/feed-conf.sh
+++ scripts/feed-conf.sh
@@ -12,23 +12,25 @@ cp -r officalpackages/lang/golang feeds/packages/lang
 rm -rf officalpackages
 
 REPOS=(
-    "https://github.com/this-username-has-been-taken/amneziawg-openwrt.git|kmod-amneziawg,amneziawg-tools,amneziawg-go,luci-proto-amneziawg"
-    "https://github.com/remittor/zapret-openwrt.git|luci-app-zapret2,zapret2"
-    # "https://github.com/other/repo.git|package1,package2,package3"
+    "https://github.com/this-username-has-been-taken/amneziawg-openwrt.git|master|kmod-amneziawg,amneziawg-tools,amneziawg-go,luci-proto-amneziawg"
+    "https://github.com/remittor/zapret-openwrt.git|master|luci-app-zapret2,zapret2"
+    # "https://github.com/other/repo.git|branch_name|package1,package2,package3"
 )
+
 for item in "${REPOS[@]}"; do
     repo_url=$(echo $item | cut -d'|' -f1)
-    packages=$(echo $item | cut -d'|' -f2)
+    branch=$(echo $item | cut -d'|' -f2)
+    packages=$(echo $item | cut -d'|' -f3)
     repo_name=$(basename $repo_url .git)
-    echo "Обрабатываем $repo_name..."
-    git clone --depth=1 --single-branch $repo_url
+    echo "Обрабатываем $repo_name (ветка: $branch)..."
+    git clone --depth=1 --single-branch --branch "$branch" "$repo_url"
     mkdir -p package/openwrt-packages
     IFS=',' read -ra PKG_ARRAY <<< "$packages"
     for pkg in "${PKG_ARRAY[@]}"; do
         echo "  Копируем $pkg"
-        cp -rf $repo_name/$pkg package/openwrt-packages/
+        cp -rf "$repo_name/$pkg" package/openwrt-packages/
     done
-    rm -rf $repo_name
+    rm -rf "$repo_name"
 done
 
 ./scripts/feeds install -a
