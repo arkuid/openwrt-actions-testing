d2c1bbac5e044a2b95d0614971752f237c3be65a
arkuid <149357139+arkuid@users.noreply.github.com>
2026-01-17 00:08:05 +0500

    Create customscript.sh

---
+++ tplink/c20v5/24/customscript.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+for p in *.patch; do patch -p1 -d feeds/packages < "$p"; done
+
+cat > mktplinkfw2.patch << 'EOF'
+--- a/tools/firmware-utils/patches/001-32mb-mktplinkfw2.patch
++++ b/tools/firmware-utils/patches/001-32mb-mktplinkfw2.patch
+@@ -0,0 +1,15 @@
++--- firmware-utils.orig/src/mktplinkfw2.c
+++++ firmware-utils/src/mktplinkfw2.c
++@@ -167,6 +167,12 @@ static struct flash_layout layouts[] = {
++ 		.kernel_la	= 0x80000000,
++ 		.kernel_ep	= 0x80000000,
++ 		.rootfs_ofs	= 0x140000,
+++	}, {
+++		.id		= "32MSUmtk",
+++		.fw_max_len	= 0x1F70000,
+++		.kernel_la	= 0x80000000,
+++		.kernel_ep	= 0x80000000,
+++		.rootfs_ofs	= 0x140000,
++ 	}, {
++ 		.id		= "8MLmtk",
++ 		.fw_max_len	= 0x7b0000,
+
+EOF
+patch -p1 < mktplinkfw2.patch
+
+sed -i '/^define Device\/tplink_archer-c20-v5/,/^endef/ {
+  s/IMAGE_SIZE := 7616k/IMAGE_SIZE := 32192k/
+  s/TPLINK_FLASHLAYOUT := 8MSUmtk/TPLINK_FLASHLAYOUT := 32MSUmtk/
+}' target/linux/ramips/image/mt76x8.mk
+
+sed -i 's/reg = <0x50000 0x770000>/reg = <0x50000 0x1f70000>/' target/linux/ramips/dts/mt7628an_tplink_8m-split-uboot.dtsi
+
+sed -i '
+  s/reg = <0x7c0000 0x10000>/reg = <0x1fc0000 0x10000>/
+  s/reg = <0x7d0000 0x10000>/reg = <0x1fd0000 0x10000>/
+  s/reg = <0x7e0000 0x10000>/reg = <0x1fe0000 0x10000>/
+  s/reg = <0x7f0000 0x10000>/reg = <0x1ff0000 0x10000>/
+' target/linux/ramips/dts/mt7628an_tplink_8m-split-uboot.dtsi
